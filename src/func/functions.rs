aadfb36624423b9703270dc5406c34f486b721b4
aciddm3 <dmacid3@gmail.com>
2026-05-11 23:53:21 +0700

    yet another impr

--- src/func/functions.rs
+++ src/func/functions.rs
@@ -3,12 +3,26 @@ const FRAC_3PI_2: f32 = PI + FRAC_PI_2;
 
 #[inline]
 pub fn sigmoid(x: f32) -> f32 {
-    if x >= 15.0 {
+    const S0: f32 = 6.549889643289383e-06;
+    const S1: f32 = 2.472077354175178e-01;
+    const S2: f32 = 1.290223762394391e-02;
+    const S3: f32 = -4.076700256613482e-02;
+    const S4: f32 = 1.403104754073916e-02;
+    const S5: f32 = -2.408918337689537e-03;
+    const S6: f32 = 2.317087210268684e-04;
+    const S7: f32 = -1.192241369934327e-05;
+    const S8: f32 = 2.560170922345861e-07;
+
+    if x >= 10.0 {
         1.0
-    } else if x <= -15.0 {
+    } else if x <= -10.0 {
         0.0
     } else {
-        1.0 / (1.0 + (-x).exp())
+        let (sgn, x) = (x.signum(), x.abs());
+        0.5 + sgn
+            * (S0
+                + x * (S1
+                    + x * (S2 + x * (S3 + x * (S4 + x * (S5 + x * (S6 + x * (S7 + x * S8))))))))
     }
 }
 
@@ -16,7 +30,7 @@ pub fn sigmoid(x: f32) -> f32 {
 pub fn approx_sine(mut x: f32) -> f32 {
     x = x.rem_euclid(TAU);
 
-    let (x_reduced, sign) = if x <= PI {
+    let (x_reduced, sgn) = if x <= PI {
         if x <= FRAC_PI_2 {
             (x, 1.0)
         } else {
@@ -30,21 +44,36 @@ pub fn approx_sine(mut x: f32) -> f32 {
         }
     };
 
-    sign * sine_poly(x_reduced)
+    sgn * sine_poly(x_reduced)
 }
 
 #[inline]
-pub fn approx_cosine(x: f32) -> f32 {
-    approx_sine(x + FRAC_PI_2)
-}
+pub fn approx_cosine(mut x: f32) -> f32 {
+    x = x.rem_euclid(TAU);
 
-const SINE_0: f32 = 1.168_518_1e-05; // 0. coef in sine function
-const SINE_1: f32 = 9.996_429_7e-01; // 1. coef in sine function
-const SINE_2: f32 = 2.296_591_9e-03; // 2. coef in sine function
-const SINE_3: f32 =-1.722_893_6e-01; // 3. coef in sine function
-const SINE_4: f32 = 6.067_09e-03; // 4. coef in sine function
-const SINE_5: f32 = 5.742_717_5e-03; // 5. coef in sine function
+    let (x_reduced, sgn) = if x <= PI {
+        if x <= FRAC_PI_2 {
+            (FRAC_PI_2 - x, 1.0)
+        } else {
+            (x - FRAC_PI_2, -1.0)
+        }
+    } else {
+        if x <= FRAC_3PI_2 {
+            (FRAC_3PI_2 - x, -1.0)
+        } else {
+            (x - FRAC_3PI_2, 1.0)
+        }
+    };
+
+    sgn * sine_poly(x_reduced)
+}
 #[inline]
 fn sine_poly(x: f32) -> f32 {
+    const SINE_0: f32 = 1.168_518_1e-05;
+    const SINE_1: f32 = 9.996_429_7e-01;
+    const SINE_2: f32 = 2.296_591_9e-03;
+    const SINE_3: f32 = -1.722_893_6e-01;
+    const SINE_4: f32 = 6.067_09e-03;
+    const SINE_5: f32 = 5.742_717_5e-03;
     SINE_0 + x * (SINE_1 + x * (SINE_2 + x * (SINE_3 + x * (SINE_4 + x * SINE_5))))
 }
--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,5 +1,5 @@
 // parser/error.rs
-use std::fmt;
+use std::fmt::{self};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct ParseError {
@@ -24,16 +24,42 @@ pub enum ErrorKind {
     ConversionError(String),
 }
 
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Ошибка парсинга [{}:{}]: {:?}\n\t -> {}",
-            self.line, self.column, self.kind, self.snippet
-        )
-    }
-}
-
 impl std::error::Error for ParseError {}
 
 pub type ParseResult<T> = Result<T, ParseError>;
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let error_desc = match &self.kind {
+            ErrorKind::UnexpectedChar(c) => format!("unexpected character '{}'", c),
+            ErrorKind::UnexpectedEof => "unexpected end of file".to_string(),
+            ErrorKind::InvalidNumber(s) => format!("invalid number \"{}\"", s),
+            ErrorKind::UnknownSymbol(s) => format!("unknown symbol \"{}\"", s),
+            ErrorKind::WrongArity {
+                symbol,
+                expected,
+                got,
+            } => {
+                format!(
+                    "\"{}\" expected {} argument(s), got {}",
+                    symbol, expected, got
+                )
+            }
+            ErrorKind::MismatchedParens => "mismatched parentheses".to_string(),
+            ErrorKind::ConversionError(e) => format!("conversion error: {}", e),
+        };
+
+        write!(f, "error: {}|{}: {}", self.line, self.column, error_desc)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::parser::parse_func;
+
+    #[test]
+    fn check_error() {
+        let a = parse_func("(* (+ arg .-) 0.3)").unwrap_err();
+        println!("{a}");
+    }
+}
